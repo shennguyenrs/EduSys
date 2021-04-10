b9b217833f70178b08bd9fe29bf895c9273eb13e
Sang Nguyen Dinh <ericknick@Sangs-MacBook-Pro.local>
2021-04-10 14:12:59 +0300

    change the directory

---
+++ test/Test_User_Info.java
@@ -0,0 +1,14 @@
+package test;
+
+import src.FunctionClass.*;
+
+public class Test_User_Info {
+    public static void main (String[] args){
+        try{
+            UserInfo.PrintUserInfo();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
